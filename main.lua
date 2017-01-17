6924815e5d475591375b3f6872cb48168d1e5c3e
Rob Miracle <rob@coronalabs.com>
2017-01-16 21:04:46 -0500

    Initial release

---
+++ main.lua
@@ -0,0 +1,6 @@
+local helloWorld = display.newText( "Hello World", display.contentCenterX,display.contentCenterY, system.fontBold, 32 )
+helloWorld:setFillColor( 1 )
+
+local dimensions = display.newText( tostring(display.pixelWidth) .. " X " .. tostring(display.pixelHeight), display.contentCenterX, display.contentCenterY + 50, system.fontBold, 24) 
+
+
